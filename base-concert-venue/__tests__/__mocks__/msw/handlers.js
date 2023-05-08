14afbbc0d57483f1b03e92b96acb61d71f0c080c
szabikr <szabikr.dev@gmail.com>
2023-05-08 08:24:18 +0300

    tests empty user reservations and sold out tickets

--- base-concert-venue/__tests__/__mocks__/msw/handlers.js
+++ base-concert-venue/__tests__/__mocks__/msw/handlers.js
@@ -1,4 +1,5 @@
 import { rest } from "msw";
+
 import { readFakeData } from "@/__tests__/__mocks__/fakeData";
 import { fakeUserReservations } from "@/__tests__/__mocks__/fakeData/userReservations";
 
@@ -7,12 +8,22 @@ export const handlers = [
     "http://localhost:3000/api/shows/:showId",
     async (req, res, context) => {
       const { fakeShows } = await readFakeData();
-      return res(context.json({ show: fakeShows[0] }));
+      const { showId } = req.params;
+
+      // index / showId = 0 has seats available in fake data
+      // index / showId = 1 has NO seats available
+      return res(context.json({ show: fakeShows[Number(showId)] }));
     }
   ),
   rest.get(
     "http://localhost:3000/api/users/:userId/reservations",
-    async (req, res, context) =>
-      res(context.json({ userReservations: fakeUserReservations }))
+    async (req, res, context) => {
+      const { userId } = req.params;
+      return res(
+        context.json({
+          userReservations: Number(userId) === 0 ? [] : fakeUserReservations,
+        })
+      );
+    }
   ),
 ];
--- base-concert-venue/__tests__/ui/userReservations.test.tsx
+++ base-concert-venue/__tests__/ui/userReservations.test.tsx
@@ -11,3 +11,17 @@ test("User Reservations page displays correct number of reservations", async () 
 
   expect(ticketsButtonText).toBeInTheDocument();
 });
+
+test("User Reservations page displays purchase tickets button and no heading for your tickets", async () => {
+  render(<UserReservations userId={0} />);
+
+  const ticketsButton = await screen.findByRole("button", {
+    name: /purchase tickets/i,
+  });
+  expect(ticketsButton).toBeInTheDocument();
+
+  const yourTicketsHeading = screen.queryByRole("heading", {
+    name: /your tickets/i,
+  });
+  expect(yourTicketsHeading).not.toBeInTheDocument();
+});
