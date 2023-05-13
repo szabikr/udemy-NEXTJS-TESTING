31437f08c9ab03a1871d600f67af79b7161c49c4
szabikr <szabikr.dev@gmail.com>
2023-05-13 08:16:39 +0300

    Cypress tests for dynamic routes

--- base-concert-venue/cypress/e2e/routes.cy.js
+++ base-concert-venue/cypress/e2e/routes.cy.js
@@ -1,3 +1,6 @@
+import { generateNewBand } from "../../__tests__/__mocks__/fakeData/newBand";
+import { generateRandomId } from "../../lib/features/reservations/utils";
+
 it("displays correct heading when navigating to shows route", () => {
   cy.visit("/");
   cy.findByRole("button", { name: /shows/i }).click();
@@ -10,4 +13,21 @@ it("displays correct heading when navigating to bands route", () => {
   cy.findByRole("heading", { name: /our illustrious performers/i }).should(
     "exist"
   );
+});
+
+it("displays correct band name for band route that existed at build time", () => {
+  cy.task("db:reset").visit("/bands/1");
+  cy.findByRole("heading", { name: /shamrock pete/i }).should("exist");
+});
+
+it("displays band not found error for requesting a band that does not exist", () => {
+  cy.task("db:reset").visit("/bands/12345");
+  cy.findByRole("heading", { name: /error: band not found/i }).should("exist");
+});
+
+it("display name for band that was not preset at build time", () => {
+  const bandId = generateRandomId();
+  const newBand = generateNewBand(bandId);
+  cy.task("db:reset").task("addBand", newBand).visit(`/bands/${bandId}`);
+  cy.findByRole("heading", { name: /avalanche of cheese/i }).should("exist");
 });
--- base-concert-venue/cypress/plugins/index.js
+++ base-concert-venue/cypress/plugins/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 /// <reference types="cypress" />
 // ***********************************************************
 // This example plugins/index.js can be used to load plugins
@@ -12,11 +13,16 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
+const { resetDb } = require("../../__tests__/__mocks__/db/utils/reset-db");
+const { addBand } = require("../../lib/features/bands/queries");
+
 /**
  * @type {Cypress.PluginConfig}
  */
 // eslint-disable-next-line no-unused-vars
 module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
-}
+  on("task", {
+    "db:reset": () => resetDb().then(() => null),
+    addBand: (newBand) => addBand(newBand).then(() => null),
+  });
+};
